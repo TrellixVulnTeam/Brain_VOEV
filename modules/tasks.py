b207a822087eece420535fb6d98ed342ad513e32
DanielATucker <DanielATucker@Outlook.com>
2022-03-27 10:12:27 -0400

    Minor fixes

--- modules/tasks.py
+++ modules/tasks.py
@@ -6,7 +6,7 @@ def main(log, graph, username):
 
     log.info("Here are your tasks:\n")
 
-    tasks_in = graph.run("MATCH (t: Task), (u: User), (T: TaskMaster) WHERE u.name = '{username}' AND T.name = 'TaskMaster' AND (t)-[*]->(T)-[*]->(u) RETURN t", username=username).data()
+    tasks_in = graph.run(f"MATCH (t: Task), (u: User), (T: TaskMaster) WHERE u.name = '{username}' AND T.name = 'TaskMaster' AND (t)-[*]->(T)-[*]->(u) RETURN t", username=username).data()
 
     task_tree = Tree("Tasks")
 
@@ -15,14 +15,16 @@ def main(log, graph, username):
 
     for task in tasks_in:
 
-        task = task["n"]
+        task = task["t"]
         taskname = task["name"]
 
-        relationship = graph.run(f"MATCH (t: Task)-[r]->(T)-[*]->(u), (u: User), (T: TaskMaster) WHERE u.name = '{username}' AND T.name = 'TaskMaster' WHERE t.name = '{taskname}' RETURN type(r)", taskname=taskname, username=username).data()
+        relationship = graph.run(f"MATCH (t: Task)-[r]->(T)-[*]->(u), (u: User), (T: TaskMaster) WHERE u.name = '{username}' AND T.name = 'TaskMaster' AND t.name = '{taskname}' RETURN type(r)", taskname=taskname, username=username).data()
+        
         relationship = relationship[0]["type(r)"]
 
-        parent = graph.run(f"MATCH (t: Task)-[r]->(T)-[*]->(u), (u: User), (T: TaskMaster) WHERE u.name = '{username}' AND T.name = 'TaskMaster' WHERE t.name = '{taskname}' RETURN b", taskname=taskname, username=username).data()
-        parent = parent[0]["b"]["name"]
+        parent = graph.run(f"MATCH (t: Task)-[r]->(T)-[*]->(u), (u: User), (T: TaskMaster) WHERE u.name = '{username}' AND T.name = 'TaskMaster' AND t.name = '{taskname}' RETURN t", taskname=taskname, username=username).data()
+        
+        parent = parent[0]["t"]["name"]
 
         task_branch_name = taskname + "_Branch"
 
@@ -34,6 +36,7 @@ def main(log, graph, username):
                     task_branch_names[parent_branch_name] = parent_branch_name
 
                     grandparent = graph.run(f"MATCH (t)-[r]->(b)-[r2]->(c)-[*]->(T)-[*]->(u) WHERE t.name = '{taskname}' AND T.name = 'TaskMaster' AND u.name = '{username}' RETURN c", taskname=taskname, username=username).data()
+
                     grandparent = grandparent[0]["c"]["name"]
                     grandparent_branch_name = grandparent + "_Branch"
 
