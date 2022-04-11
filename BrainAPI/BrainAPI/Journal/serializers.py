180e50c0bdbc5e8e385c303114c461d8d63db497
Daniel Tucker <DanielATucker@Outlook.com>
2022-04-11 01:22:28 -0400

    Deleted unnessasary files.

--- BrainAPI/BrainAPI/Journal/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Journal
-
-class JournalsSerializer(serializers.ModelSerializer):
-
-
-    class Meta:
-        model = Journal
-        fields = ('anxiety', 'depression', 'energy', 'mood')
